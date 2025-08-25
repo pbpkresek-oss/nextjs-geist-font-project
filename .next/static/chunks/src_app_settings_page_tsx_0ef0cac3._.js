a09633efb9a50834982c95868ecfcf653568b483
pbpkresek-oss <pbpkresek@gmail.com>
2025-08-25 17:22:28 +0700

    Add .next/static/chunks/src_app_settings_page_tsx_0ef0cac3._.js 1756117347428

---
+++ .next/static/chunks/src_app_settings_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a31de8ea._.js",
+  "static/chunks/src_017a7265._.js"
+],
+    source: "dynamic"
+});
